fca98fcdd73b73fc90e1687a4561657dff27b4e8
douglasr136-oss <douglasr136@gmail.com>
2025-12-14 15:52:42 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 const pool = require('./config/database')
 const authRoutes = require('./routes/auth')
+app.use('/auth', authRoutes)
 const setupRoutes = require('./routes/setup')
 
 const app = express()
