0392ea77824ac87bc33c786ab32e9682c0e0bd8f
douglasr136-oss <douglasr136@gmail.com>
2025-12-18 08:11:18 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,21 +6,19 @@ const m3uRoutes = require('./routes/m3u')
 
 const app = express()
 
-// Middlewares
 app.use(cors())
 app.use(express.json())
 
-// Rota de teste (healthcheck)
-app.get('/', (req, res) => {
-  res.send('VisionStream Backend rodando 🚀')
-})
-
-// Rotas
 app.use('/', m3uRoutes)
 
-// Porta
-const PORT = process.env.PORT || 3000
+app.get('/', (req, res) => {
+  res.json({
+    name: 'VisionStream API',
+    status: 'online'
+  })
+})
 
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`)
+  console.log(`API rodando na porta ${PORT}`)
 })
