9736f55434470b06336f35d3c5cb64ffbd54e657
douglasr136-oss <douglasr136@gmail.com>
2025-12-19 19:57:04 -0300

    Update authMiddleware.js

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -4,34 +4,26 @@ module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization
 
   if (!authHeader) {
-    return res.status(401).json({
-      error: 'Token não informado'
-    })
+    return res.status(401).json({ error: 'Token não informado' })
   }
 
   const parts = authHeader.split(' ')
 
   if (parts.length !== 2) {
-    return res.status(401).json({
-      error: 'Token mal formatado'
-    })
+    return res.status(401).json({ error: 'Token mal formatado' })
   }
 
   const [scheme, token] = parts
 
   if (!/^Bearer$/i.test(scheme)) {
-    return res.status(401).json({
-      error: 'Token mal formatado'
-    })
+    return res.status(401).json({ error: 'Token mal formatado' })
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    req.user = decoded
+    req.reseller = decoded
     return next()
   } catch (err) {
-    return res.status(401).json({
-      error: 'Token inválido'
-    })
+    return res.status(401).json({ error: 'Token inválido' })
   }
 }
